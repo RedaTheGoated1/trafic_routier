d7406cec558927054ff1fefc2fcd4677e84c66c8
ilies78 <80094926+ilies78@users.noreply.github.com>
2021-04-30 18:19:05 +0200

    Update trafic_routier.py

--- trafic_routier.py
+++ trafic_routier.py
@@ -1,38 +1,94 @@
 import numpy as np
 
-max_iter=10
+max_iter=100
 taille=3
-longueur=12
-affluence=0.4
+longueur=25
+nvoit=50
 
+
+"""creation route aléatoire"""
 def creation_route(taille):
     route=np.zeros((longueur,taille),dtype=int)
+    a=True
+    for i in range(0,nvoit):
+        while a:
+            x=np.random.choice(range(longueur))
+            y=np.random.choice(range(taille))
+            if route[x][y]==0:
+                route[x][y]=1
+                a=False
+        a=True
     return route
 
+"""affichage de la route"""
+def affiche(route):
+    r=np.zeros((longueur,taille),dtype=str)
+    for i in range(0,taille):
+        for j in range(0,longueur):
+            if route[j][i]==0:
+                r[j][i]="_"
+            elif route[j][i]==2:
+                r[j][i]="X"
+            elif route[j][i]==1: 
+                r[j][i]="🚗"
+    print(r)
 
-def trafic_routier(route):
+
+
+    
+    
+"""simulation du traffic routier"""
+def trafic_routier(route) :
+    x=0
     for i in range(0,max_iter):
-        
-            for j in range(0,taille):
-                if route[longueur-1,j]==0:
-                    route[longueur-1,j]=np.random.choice([0,1],p=[1-affluence,affluence]) 
+        for k in range(0,longueur):
+            for l in range(0,taille):
+                
+                if k==0 and route[k,l]==1 and route[longueur-1,l]==0:
+                    route[k,l]=0
+                    route[longueur-1,l]=1
                     
-            for k in range(0,longueur):
-                for l in range(0,taille):
-                    if route[k-1,l]==0 and route[k,l]==1:
-                        route[k-1,l]=1
-                        route[k,l]=0
+                elif route[k-1,l]==0 and route[k,l]==1:
+                    route[k-1,l]=1
+                    route[k,l]=0
+            
+                    
+                elif l!=0 and l!=taille-1:        
                     if route[k-1,l]==1 and route[k,l]==1:
+                    
                         if route[k,l-1]==0 and route[k-1,l-1]==0:
                             route[k,l-1]=1
                             route[k,l]=0
-                        if route[k,l+1]==0 and route[k+1,l+1]==0:
-                            route[k,l+1]=1
+                    
+                        if route[k,l+1]==0 and route[k-1,l+1]==0:
+                            route[k-1,l+1]=1
+                            route[k,l]=0
+                elif l==0:
+                    if route[k,l+1]==0 and route[k-1,l+1]==0:
+                            route[k-1,l+1]=1
                             route[k,l]=0
                             
-            for m in range(0,taille):
-                if route[0,m]==1:
-                    route[0,m]=0
+                elif l==taille:
+                    if route[k,l-1]==0 and route[k-1,l-1]==0:
+                            route[k-1,l-1]=1
+                            route[k,l]=0
+                    
+                    
+        """"bouchon aléatoire"""  
+        if x==10:
+            a=True
+            while a:
+                x=np.random.choice(range(longueur))
+                y=np.random.choice(range(taille))
+                if route[x][y]==1:
+                    route[x][y]=2
+                    a=False
             
-            print(route)
-            
+        x=x+1
+        affiche(route)
+        input(f"\nACTUALISATION {i} : \n")
+    return x
+
+
+route=creation_route(taille)
+trafic_routier(route)
